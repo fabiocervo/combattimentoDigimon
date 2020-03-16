e18a4d5d4b27a65342cad9232d8717e0442ab649
ilbob91 <massimo.ziccardi91@gmail.com>
2020-03-16 12:55:03 +0100

    va finito il main

--- combattimentodigimon/src/main/java/combattimentodigimon/MainArena.java
+++ combattimentodigimon/src/main/java/combattimentodigimon/MainArena.java
@@ -23,33 +23,48 @@ public class MainArena {
 
 	}
 
-	public static void chiamaPartita(Connection connessione, Scanner scanner)throws SQLException {
+	public static void chiamaPartita(Connection connessione, Scanner scanner, GestioneTurniDigimon g)
+			throws SQLException, ClassNotFoundException {
 		Statement statement = connessione.createStatement();
 		ResultSet risultato = statement.executeQuery("SELECT idpartita and password FROM ExmDFToJYb.partite;");
-		
-        List<Integer> elencoPartite = new ArrayList<>();
-        while (risultato.next()) {
-            int id = risultato.getInt(1);
-            String password = risultato.getString(2);
-            elencoPartite.add(id);
-            System.out.println(id + ". " + password);
-            ;
-        }
-        System.out.println("qual è l 'id ?");
-        int id = scanner.nextInt();
-        scanner.nextLine();
-        if (elencoPartite.contains(id)) {
-		
-        	PreparedStatement prepareStatement = connessione.prepareStatement ("select partite.idcreatore, partite.dc1,digimon.nome,digimon.atk,digimon.def,digimon.res,digimon.hp, digimon.evo, digimon.tipo, from partite inner join digimon on digimon.id = partite.dc1 and partite.idpartita = ?;");
-		prepareStatement.setInt(1, id);
-		ResultSet risultato2 = prepareStatement.executeQuery();
-		
-		
+
+		List<Integer> elencoPartite = new ArrayList<>();
+		while (risultato.next()) {
+			int id = risultato.getInt(1);
+			String password = risultato.getString(2);
+			elencoPartite.add(id);
+			System.out.println(id + ". " + password);
+			;
+		}
+		System.out.println("qual è l 'id ?");
+		int id = scanner.nextInt();
+		scanner.nextLine();
+
+		if (elencoPartite.contains(id)) {
+
+			PreparedStatement prepareStatement = connessione.prepareStatement(
+					"select partite.idcreatore, partite.dc1,digimon.nome,digimon.atk,digimon.def,digimon.res,digimon.hp, digimon.evo, digimon.tipo, from partite inner join digimon on digimon.id = partite.dc1 and partite.idpartita = ?;");
+			prepareStatement.setInt(1, id);
+			ResultSet risultato2 = prepareStatement.executeQuery();
+			while (risultato2.next()) {
+				String idCreatore = risultato2.getString(1);
+				String nome = risultato2.getString(3);
+				int attacco = risultato2.getInt(4);
+				int difesa = risultato2.getInt(5);
+				int res = risultato2.getInt(6);
+				int hp = risultato2.getInt(7);
+				String evo = risultato2.getString(8);
+				String tipo = risultato2.getString(9);
+				
+				Digimon d = new Digimon(nome, attacco, difesa, res, hp, evo, tipo, idCreatore);
+				g.getListaDigimonCreatore().add(d);
+			}
+
+		}
+
+		else {
+			System.out.println("Errore, riprova");
+		}
+
 	}
-        
-        else {
-		System.out.println("Errore, riprova");
-	} 
-        
-} 
 }
