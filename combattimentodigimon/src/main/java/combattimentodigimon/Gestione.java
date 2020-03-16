b716041556e11a9be5a961163ebac02b71598fd7
pappa22 <marta.yiannakou@gmail.com>
2020-03-16 19:34:48 +0100

    gest method partita

--- combattimentodigimon/src/main/java/combattimentodigimon/Gestione.java
+++ combattimentodigimon/src/main/java/combattimentodigimon/Gestione.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class Gestione {
+public class Gestione { 
 	private Connection connessione;
 
 	public Connection getConnessione() {
